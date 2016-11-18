93749febe5cb8bd673b19d32369b4fa8690d7709
joakimthun <joakimthun@gmail.com>
2016-11-18 07:08:42 +0100

    added sdl_image

--- src/elsa/elsa.cpp
+++ src/elsa/elsa.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
 
 #include "errors/elsa_exception.h"
@@ -53,6 +54,13 @@ int main(int argc, char* args[])
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         std::cout << "SDL could not be initialized! SDL_Error:" << std::endl << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    auto img_flags = IMG_INIT_PNG;
+    if (!(IMG_Init(img_flags) & img_flags))
+    {
+        std::cout << "SDL_image could not be initialized! IMG_GetError:" << std::endl << IMG_GetError() << std::endl;
         return 1;
     }
 
